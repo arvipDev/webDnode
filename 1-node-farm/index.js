aa0a704d90965a52f47294aa9e0743ecc9306513
arvipDev <arvind.purushotham@gmail.com>
2024-01-12 01:03:50 -0500

    http server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 
 // -----This is the blocking, synchronous way
 // const input = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -8,19 +9,27 @@ const fs = require('fs');
 // const output = `Some text I want to write to the file system: ${input} .\n created on: ${Date.now}`;
 // fs.writeFileSync('./txt/output.txt', output);
 
-//This is the asynchronous, non-blocking way
-//Note the triangular space indentation on the left, it is called callback hell.
-//There is a way to make the process asynchoronous without using callback/causing callback hell.
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-    if(err) return console.log('Error!!!');
-    fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt','utf-8',(err, data3) => {
-            console.log(data3);
-            fs.writeFile('./txt/final.txt',`${data2}.\n${data3}` ,'utf-8',(err, data4) => {
-                console.log("File successfully written");
-            })
-        })
-    });
+// //This is the asynchronous, non-blocking way
+// //Note the triangular space indentation on the left, it is called callback hell.
+// //There is a way to make the process asynchoronous without using callback/causing callback hell.
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+//     if(err) return console.log('Error!!!');
+//     fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt','utf-8',(err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt',`${data2}.\n${data3}` ,'utf-8',(err, data4) => {
+//                 console.log("File successfully written");
+//             })
+//         })
+//     });
+// });
+// console.log('Reading file...');
+
+const server = http.createServer((req, res) => {
+    res.end('hello fron the server!');
 });
-console.log('Reading file...');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000: ');
+});
